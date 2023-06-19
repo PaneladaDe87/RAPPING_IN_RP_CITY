42edb4251968137e2c58a33695e53fbf3c9a856c
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2023-06-18 21:08:35 -0300

    Create IntroState.hx

---
+++ source/IntroState.hx
@@ -0,0 +1,22 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxState;
+
+public class IntroState extends FlxState {
+    private var IntroEnded:Bool;
+
+    override function create():Void {
+        super.create();
+    }
+    
+    override function update(elapsed:Float):Void {
+        if(FlxG.keys.space()) {
+            IntroEnded = true;
+        }
+
+        if(IntroEnded = true) {
+            FlxG.switchState(new MenuState());
+        }
+    }
+}
